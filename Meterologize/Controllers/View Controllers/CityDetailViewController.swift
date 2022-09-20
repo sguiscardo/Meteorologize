9e27372dc04bcc583030327cfaaaa730fff9b027
Karl Pfister <kpfister@me.com>
2022-09-20 09:08:26 -0600

    Resets app to day 1 status

--- Meterologize/Controllers/View Controllers/CityDetailViewController.swift
+++ Meterologize/Controllers/View Controllers/CityDetailViewController.swift
@@ -9,22 +9,23 @@ import UIKit
 
 class CityDetailViewController: UIViewController {
 
+    //MARK: - Outlets
     @IBOutlet weak var cityNameLabel: UILabel!
     @IBOutlet weak var currentStatusLabel: UILabel!
     @IBOutlet weak var currentTempLabel: UILabel!
     @IBOutlet weak var projectedHighLabel: UILabel!
     @IBOutlet weak var projectedLowLabel: UILabel!
     
-    var city: City?
-    
+    //MARK: - View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         updateViews()
         // Do any additional setup after loading the view.
     }
     
+    //MARK: - Methods
     func updateViews() {
-        guard let city = city else {return}
+        let city = CityController.sharedInstance.cities[0]
         self.view.backgroundColor = city.currentTemp <= 80.0 ? .cyan : .red
         cityNameLabel.text = city.name
         currentStatusLabel.text = city.currentStatus
@@ -32,4 +33,4 @@ class CityDetailViewController: UIViewController {
         projectedHighLabel.text = "High: \(city.dailyHigh)"
         projectedLowLabel.text = "Low: \(city.dailyHigh)"
     }
-}
+} // End of Class
