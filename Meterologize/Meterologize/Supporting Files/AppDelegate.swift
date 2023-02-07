df0e0977826b12d3be43f8793b8518aefea7050b
max poffenbarger <maxpoffenbarger@gmail.com>
2023-02-07 14:19:06 -0700

    Main Overhaul
    DS Store issues were creating parsing problems with the project file. Because day 1 and day 3 will be updated as well using this base project, it is more efficient to wipe clean than to sort through all the parsing, or redo all the work on the original project

--- Meterologize/Meterologize/Supporting Files/AppDelegate.swift
+++ Meterologize/Meterologize/Supporting Files/AppDelegate.swift
@@ -2,7 +2,7 @@
 //  AppDelegate.swift
 //  Meterologize
 //
-//  Created by Karl Pfister on 8/19/21.
+//  Created by Maxwell Poffenbarger on 2/6/23.
 //
 
 import UIKit
@@ -12,25 +12,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        
-        return true
-    }
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+		// Override point for customization after application launch.
 
-    // MARK: UISceneSession Lifecycle
+		return true
+	}
 
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
+	// MARK: UISceneSession Lifecycle
 
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
+	func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+		// Called when a new scene session is being created.
+		// Use this method to select a configuration to create the new scene with.
+		return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+	}
+
+	func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+		// Called when the user discards a scene session.
+		// If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+		// Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+	}
 
 
 }
